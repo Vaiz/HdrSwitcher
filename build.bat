02dfe27d9782e7e6cea637b4443f31100cd8a59c
Vaiz <vaizerd@gmail.com>
2023-12-05 12:08:49 +0100

    add build script

---
+++ build.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+
+REM Change to your project's root directory, if necessary
+cd /d %~dp0
+
+REM Configure the project using the x64-release preset
+cmake -S . -B out/build/x64-release --preset x64-release
+
+REM Build the project
+cmake --build out/build/x64-release
+
+echo Build completed.
+endlocal
