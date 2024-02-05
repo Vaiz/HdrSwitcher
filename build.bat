f2db58671360abb47239438fc8f098c770dd75e3
Vaiz <vaizerd@gmail.com>
2024-02-05 08:52:58 +0100

    reduce release binary size

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ REM Change to your project's root directory, if necessary
 cd /d %~dp0
 
 REM Configure the project using the x64-release preset
-cmake -S . -B out/build/x64-release --preset x64-release
+cmake --fresh -S . -B out/build/x64-release --preset x64-release
 
 REM Build the project
 cmake --build out/build/x64-release
