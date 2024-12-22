44619e48a105da906c7a5316e685b22306424dfa
Vaiz <4908982+Vaiz@users.noreply.github.com>
2024-12-22 19:36:34 +0100

    1.3 dev (#5)
    - move source code to src dir
    - move logic to Display class
    - print HDR status instead of adapter path
    - switch to C++23

---
+++ src/display.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "winapi.h"
+
+enum HdrStatus {
+  NotSupported,
+  Enabled,
+  Disabled,
+};
+
+class Display {
+  LUID adapterId;
+  std::uint32_t targetId;
+  HdrStatus hdrStatus;
+  std::wstring name;
+
+ public:
+  Display(const DISPLAYCONFIG_PATH_INFO& displayInfo)
+      : adapterId(displayInfo.sourceInfo.adapterId),
+        targetId(displayInfo.targetInfo.id) {
+    updateHdrStatus();
+
+    auto targetName = GetDisplayName(adapterId, targetId);
+    name = (targetName.flags.friendlyNameFromEdid
+                ? targetName.monitorFriendlyDeviceName
+                : L"Unknown");
+  }
+
+  const std::wstring& getName() const { return name; }
+  const std::uint32_t getTargetId() const { return targetId; }
+  const LUID getAdapterId() const { return adapterId; }
+
+  void enableHdr() {
+    SetAdvancedColorInfo(adapterId, targetId, true);
+    updateHdrStatus();
+  }
+  void disableHdr() {
+    SetAdvancedColorInfo(adapterId, targetId, false);
+    updateHdrStatus();
+  }
+  void toggleHdr() {
+    if (!isHdrSupported()) {
+      throw std::runtime_error("The display does not support HDR");
+    }
+    SetAdvancedColorInfo(adapterId, targetId, !isHdrEnabled());
+    updateHdrStatus();
+  }
+  HdrStatus getHdrStatus() const { return hdrStatus; }
+  bool isHdrEnabled() const { return hdrStatus == HdrStatus::Enabled; }
+  bool isHdrSupported() const { return hdrStatus != HdrStatus::NotSupported; }
+
+  static vec<Display> QueryAllDisplays() {
+    return QueryDisplayConfigImpl() |
+           std::views::transform(
+               [](const auto& info) { return Display(info); }) |
+           std::ranges::to<std::vector>();
+  }
+
+ private:
+  void updateHdrStatus() {
+    auto getColorInfo = GetAdvancedColorInfo(adapterId, targetId);
+    if (!getColorInfo.advancedColorSupported) {
+      hdrStatus = HdrStatus::NotSupported;
+    } else if (getColorInfo.advancedColorEnabled) {
+      hdrStatus = HdrStatus::Enabled;
+    } else {
+      hdrStatus = HdrStatus::Disabled;
+    }
+  }
+};
---
+++ src/filters.h
@@ -0,0 +1,68 @@
+#pragma once
+#include "display.h"
+#include "winapi.h"
+
+struct IFilter {
+  virtual ~IFilter() = default;
+  virtual vec<Display> Apply(vec<Display> displays) const = 0;
+};
+
+struct AllFilter final : IFilter {
+  vec<Display> Apply(vec<Display> displays) const { return displays; }
+};
+
+struct IndexFilter final : IFilter {
+  size_t index;
+
+  IndexFilter(size_t index) : index(index) {}
+
+  vec<Display> Apply(vec<Display> displays) const {
+    if (displays.size() <= index) {
+      throw std::logic_error(format(
+          "Cannot find display with index {}. There is/are only {} display(s)",
+          index, displays.size()));
+    }
+
+    return {displays.at(index)};
+  }
+};
+
+struct TargetIdFilter final : IFilter {
+  uint32_t targetId;
+
+  TargetIdFilter(uint32_t targetId) : targetId(targetId) {}
+
+  vec<Display> Apply(vec<Display> displays) const {
+    auto iter = std::ranges::find_if(displays, [this](const Display& display) {
+      return display.getTargetId() == targetId;
+    });
+
+    if (displays.end() == iter) {
+      throw std::logic_error(
+          format("Cannot find display with target id {}", targetId));
+    }
+
+    return {*iter};
+  }
+};
+
+struct DisplayNameFilter final : IFilter {
+  std::string displayName;
+
+  DisplayNameFilter(std::string displayName)
+      : displayName(to_upper(displayName)) {}
+
+  vec<Display> Apply(vec<Display> displays) const {
+    auto view = displays | std::views::filter([this](const Display& display) {
+                  auto name = display.getName();
+                  return to_upper(to_utf8(name)) == displayName;
+                });
+
+    if (view.empty()) {
+      throw std::logic_error(
+          format("Cannot find display with name [{}]", displayName));
+    }
+
+    return view | std::ranges::to<std::vector>();
+  }
+};
